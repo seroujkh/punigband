3533080f03d03da614b2a8b625f9a51317fec57b
seroujkh <rafayel28@hotmail.com>
2020-07-31 20:28:55 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+var swiper = new Swiper('.swiper-container', {
+    allowTouchMove: true,
+    slidesPerView: 1.1,
+    paginationClickable: true,
+    preventClicks: true,
+    centeredSlides: true,
+    spaceBetween: 5,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: true
+    },
+    pagination: {
+        el: null
+    },
+    breakpoints: {
+        1199: {
+            slidesPerView: 2,
+            spaceBetween: 18,
+        },
+        991: {
+            slidesPerView: 2,
+            spaceBetween: 18,
+        },
+        767: {
+            slidesPerView: 1.2,
+            spaceBetween: 18,
+        },
+        500: {
+            slidesPerView: 1.2,
+            spaceBetween: 0,
+        }
+    },
+    on: {
+        resize: function() {
+            swiperClients2.update();
+        }
+    }
+
+});
+$(function() {
+    $(document).scroll(function() {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+$('.custom-burger, .close-menu').on('click', function() {
+    var burgerMenu = $('.custom-burger-collapse');
+    burgerMenu.toggleClass('show');
+
+});
+$(window).on('load', function(e) {
+    if (window.location.hash) {
+        e.stopPropagation();
+        var hash = window.location.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - 140.66
+        }, 500);
+    }
+});
