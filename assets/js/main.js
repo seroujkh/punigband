763d5d431816700ee3809bf127b1260d7e7d93cc
unknown <seroujkh@hotmail.com>
2020-10-05 23:58:33 +0200

    Punig-New

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,40 +31,26 @@ var swiper = new Swiper('.swiper-container', {
         }
     },
     on: {
-        resize: function() {
+        resize: function () {
             swiperClients2.update();
         }
     }
 
 });
-$(function() {
-    $(document).scroll(function() {
-        var $nav = $(".navbar");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-});
-$('.custom-burger').on('click', function() {
-    $('.custom-burger').toggleClass('open');
-    var burgerMenu = $('.custom-burger-collapse');
-    burgerMenu.toggleClass('show');
 
-});
-$(document).on('click', 'a[href^="#"]', function(event) {
+
+$(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top - 70
+        scrollTop: $($.attr(this, 'href')).offset().top - 60
     }, 500);
 });
-$('.custom-burger-collapse ul li a ').on('click', function() {
-    var burgerMenu = $('.custom-burger-collapse');
-    burgerMenu.toggleClass('show');
 
-});
 
 
 //viewport
-$(window).scroll(function() {
-    $('.slide-up, .slide-down, .slide-left, .slide-right').each(function() {
+$(window).scroll(function () {
+    $('.slide-up, .slide-down, .slide-left, .slide-right').each(function () {
         var top_of_element = $(this).offset().top;
         var bottom_of_element = $(this).offset().top + $(this).outerHeight();
         var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
@@ -76,3 +62,29 @@ $(window).scroll(function() {
     });
 
 }).scroll();
+
+
+var lastScrollTop = 0;
+$(window).scroll(function (event) {
+    var st = $(window).scrollTop();
+    if (st > lastScrollTop) {
+        // downscroll code
+        $('.punig-nav').addClass('scrolled');
+    } else if (st < 500) {
+        $('.punig-nav').removeClass('scrolled');
+
+    }
+
+    lastScrollTop = st;
+});
+
+$('.custom-burger').on('click', function () {
+
+    $(this).toggleClass('open');
+    $('.custom-burger-collapse').toggleClass('show');
+
+});
+$('.custom-burger-collapse li a').on('click', function () {
+    $('.custom-burger').toggleClass('open');
+    $('.custom-burger-collapse').toggleClass('show');
+})
